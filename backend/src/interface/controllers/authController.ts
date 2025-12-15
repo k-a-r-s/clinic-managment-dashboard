075e33099e89d152c2de457afb802a8bcbeae0e3
gyro-mc <gyro.coding@gmail.com>
2025-12-15 14:29:29 +0100

    updated the expiraction duration for the cookie

--- backend/src/interface/controllers/authController.ts
+++ backend/src/interface/controllers/authController.ts
@@ -15,7 +15,7 @@ export class AuthController {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
       sameSite: "strict",
-      maxAge: 60 * 60 * 1000, // 1 hour
+      maxAge: 6 * 24 * 60 * 60 * 1000, // 6 days
       path: "/",
     });
 
